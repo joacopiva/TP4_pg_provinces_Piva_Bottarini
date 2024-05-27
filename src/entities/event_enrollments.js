90f41dd90e500ae28eb05d89ba0e1619dd6d3eda
AGOSTINA BOTTARINI <47866440@ort>
2024-05-27 10:34:51 -0300

    entities

---
+++ src/entities/event_enrollments.js
@@ -0,0 +1,13 @@
+class Event_Enrollments
+{
+    id;
+    id_event;
+    id_user;
+    description;
+    registration_date_time;
+    attended;
+    observations;
+    rating;
+}
+
+export default Event_Enrollments;
---
+++ src/entities/event_tags.js
@@ -0,0 +1,8 @@
+class Event_Tags
+{
+    id;
+    id_event;
+    id_tag;
+}
+
+export default Event_Tags;
